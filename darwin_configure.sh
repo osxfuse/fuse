7d4b6b352f34a91965918434b9a65932cc3a34af
Benjamin Fleischer <fleiben@gmail.com>
2011-06-08 03:55:21 +0200

    Execute makeconf.sh to create the actual configure script in darwin_configure.sh and darwin_configure_ino64.sh

--- darwin_configure.sh
+++ darwin_configure.sh
@@ -3,4 +3,5 @@
 MACFUSE_SRCROOT=${MACFUSE_SRCROOT:-$1}
 MACFUSE_SRCROOT=${MACFUSE_SRCROOT:?}
 
+./makeconf.sh
 CFLAGS="-D__FreeBSD__=10 -D_POSIX_C_SOURCE=200112L -I$MACFUSE_SRCROOT/common -O -gdwarf-2 -arch i386 -arch ppc -arch x86_64 -isysroot /Developer/SDKs/MacOSX10.5.sdk -mmacosx-version-min=10.5" LDFLAGS="-arch i386 -arch ppc -arch x86_64 -framework CoreFoundation" ./configure --prefix=/usr/local --disable-dependency-tracking --disable-static
