02d591dca299da3cb7cabb24878f9e4173ce475b
Benjamin Fleischer <fleiben@gmail.com>
2011-06-17 14:50:14 +0200

    Run configure script only if makeconf.sh returned without any errors

--- darwin_configure.sh
+++ darwin_configure.sh
@@ -3,5 +3,5 @@
 MACFUSE_SRCROOT=${MACFUSE_SRCROOT:-$1}
 MACFUSE_SRCROOT=${MACFUSE_SRCROOT:?}
 
-./makeconf.sh
+./makeconf.sh && \
 CFLAGS="-D__DARWIN_64_BIT_INO_T=0 -D__FreeBSD__=10 -D_POSIX_C_SOURCE=200112L -I$MACFUSE_SRCROOT/common -O -gdwarf-2 -arch i386 -arch x86_64 -isysroot /Developer/SDKs/MacOSX10.6.sdk -mmacosx-version-min=10.5" LDFLAGS="-arch i386 -arch x86_64 -framework CoreFoundation" ./configure --prefix=/usr/local --disable-dependency-tracking --disable-static
