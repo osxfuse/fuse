84287113ea58d41ef2b2be316f3c3f9a8e546b4f
Benjamin Fleischer <fleiben@gmail.com>
2011-08-17 16:41:34 +0200

    Patch Darwin configure scripts to build CFLAGS and LDFLAGS depending on environment variables like SDKROOT or MACOSX_DEPLOYMENT_TARGET
    The build script will be responsible for setting the SDK version and
    deployment target depending on the targeted version of OS X.

--- darwin_configure_ino64.sh
+++ darwin_configure_ino64.sh
@@ -1,7 +1,40 @@
-#! /bin/sh
+#!/bin/sh
+# Copyrigth 2011, OSXFUSE Project
+# All rights reserved.
 
 OSXFUSE_SRCROOT=${OSXFUSE_SRCROOT:-$1}
 OSXFUSE_SRCROOT=${OSXFUSE_SRCROOT:?}
 
+OTHER_CFLAGS="-D__DARWIN_64_BIT_INO_T=1 -D__FreeBSD__=10 -D_POSIX_C_SOURCE=200112L -I$OSXFUSE_SRCROOT/common"
+FRAMEWORKS="CoreFoundation"
+
+CFLAGS="$OTHER_CFLAGS"
+LDFLAGS=""
+
+for arch in $ARCHS
+do
+    CFLAGS="$CFLAGS -arch $arch"
+    LDFLAGS="$LDFLAGS -arch $arch"
+done
+if [ -n "$SDKROOT" ]
+then
+    CFLAGS="$CFLAGS -isysroot $SDKROOT"
+fi
+if [ -n "$MACOSX_DEPLOYMENT_TARGET" ]
+then
+    CFLAGS="$CFLAGS -mmacosx-version-min=$MACOSX_DEPLOYMENT_TARGET"
+fi
+if [ -n "$OSXFUSE_MACFUSE_MODE" ]
+then
+    CFLAGS="$CFLAGS -DMACFUSE_MODE=$OSXFUSE_MACFUSE_MODE"
+fi
+for framework in $FRAMEWORKS
+do
+    LDFLAGS="$LDFLAGS -framework $framework"
+done
+
+export CFLAGS
+export LDFLAGS
+
 ./makeconf.sh && \
-CFLAGS="-D__DARWIN_64_BIT_INO_T=1 -D__FreeBSD__=10 -DMACFUSE_MODE -D_POSIX_C_SOURCE=200112L -I$OSXFUSE_SRCROOT/common -O -gdwarf-2 -arch i386 -arch x86_64 -isysroot /Developer/SDKs/MacOSX10.6.sdk -mmacosx-version-min=10.6" LDFLAGS="-arch i386 -arch x86_64 -framework CoreFoundation" ./configure --prefix=/usr/local --disable-dependency-tracking --disable-static
+./configure --prefix=/usr/local --disable-dependency-tracking --disable-static
