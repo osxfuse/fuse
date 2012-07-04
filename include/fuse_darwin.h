ccda89e5f6e207a97d828e8f821885e34ec9f3e5
Benjamin Fleischer <fleiben@gmail.com>
2012-07-04 21:36:00 +0200

    Remove unnamed POSIX semaphore implementation from headers
    FUSE file systems compiled against older version of libosxfuse will still work
    after this change.

--- include/fuse_darwin.h
+++ include/fuse_darwin.h
@@ -12,56 +12,7 @@
 extern "C" {
 #endif
 
-#include <errno.h>
 #include <stdint.h>
-#include <sys/types.h>
-#include <limits.h>
-#include <pthread.h>
-#include <time.h>
-
-/* Semaphores */
-
-struct __local_sem_t
-{
-	unsigned int    count;
-	pthread_mutex_t count_lock;
-	pthread_cond_t  count_cond;
-};
-
-typedef struct fuse_sem {
-	int id;
-	union {
-		struct __local_sem_t local;
-	} __data;
-} fuse_sem_t;
-
-#define FUSE_SEM_VALUE_MAX ((int32_t)32767)
-
-int fuse_sem_init(fuse_sem_t *sem, int pshared, unsigned int value);
-int fuse_sem_destroy(fuse_sem_t *sem);
-int fuse_sem_getvalue(fuse_sem_t *sem, unsigned int *value);
-int fuse_sem_post(fuse_sem_t *sem);
-int fuse_sem_timedwait(fuse_sem_t *sem, const struct timespec *abs_timeout);
-int fuse_sem_trywait(fuse_sem_t *sem);
-int fuse_sem_wait(fuse_sem_t *sem);
-
-#ifdef DARWIN_SEMAPHORE_COMPAT
-
-/* Caller must not include <semaphore.h> */
-
-typedef fuse_sem_t sem_t;
-
-#define sem_init(s, p, v)   fuse_sem_init(s, p, v)
-#define sem_destroy(s)      fuse_sem_destroy(s)
-#define sem_getvalue(s, v)  fuse_sem_getvalue(s, v)
-#define sem_post(s)         fuse_sem_post(s)
-#define sem_timedwait(s, t) fuse_sem_timedwait(s, t)
-#define sem_trywait(s)      fuse_sem_trywait(s)
-#define sem_wait(s)         fuse_sem_wait(s)
-
-#define SEM_VALUE_MAX       FUSE_SEM_VALUE_MAX
-
-#endif /* DARWIN_SEMAPHORE_COMPAT */
 
 /* Versioning */
 
