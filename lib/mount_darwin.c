81eb06a217d8b31fcd3b6163ab1e5ff747b80648
Benjamin Fleischer <fleiben@gmail.com>
2012-07-16 00:38:16 +0200

    Remove unnecessary double-fork in fuse_mount_core
    The mounting child process is reaped automatically because we called
    signal(SIGCHLD, SIG_IGN) before forking.

--- lib/mount_darwin.c
+++ lib/mount_darwin.c
@@ -511,9 +511,10 @@ fuse_unmount_compat22(const char *mountpoint)
 static int
 fuse_mount_core(const char *mountpoint, const char *opts)
 {
-    int fd, pid;
+    int fd;
     int result;
     char *fdnam, *dev;
+    pid_t pid;
     const char *mountprog = OSXFUSE_MOUNT_PROG;
 
     if (!mountpoint) {
@@ -669,44 +670,32 @@ mount:
     }
 
     if (pid == 0) {
+        const char *argv[32];
+        int a = 0;
 
-        pid = fork();
-        if (pid == -1) {
-            perror("fuse: fork() failed");
-            close(fd);
-            exit(1);
+        if (! fdnam)
+            asprintf(&fdnam, "%d", fd);
+
+        argv[a++] = mountprog;
+        if (opts) {
+            argv[a++] = "-o";
+            argv[a++] = opts;
         }
+        argv[a++] = fdnam;
+        argv[a++] = mountpoint;
+        argv[a++] = NULL;
 
-        if (pid == 0) {
-            const char *argv[32];
-            int a = 0;
-
-            if (! fdnam)
-                asprintf(&fdnam, "%d", fd);
-
-            argv[a++] = mountprog;
-            if (opts) {
-                argv[a++] = "-o";
-                argv[a++] = opts;
+        {
+            char title[MAXPATHLEN + 1] = { 0 };
+            u_int32_t len = MAXPATHLEN;
+            int ret = proc_pidpath(getpid(), title, len);
+            if (ret) {
+                setenv("MOUNT_FUSEFS_DAEMON_PATH", title, 1);
             }
-            argv[a++] = fdnam;
-            argv[a++] = mountpoint;
-            argv[a++] = NULL;
-
-            {
-                char title[MAXPATHLEN + 1] = { 0 };
-                u_int32_t len = MAXPATHLEN;
-                int ret = proc_pidpath(getpid(), title, len);
-                if (ret) {
-                    setenv("MOUNT_FUSEFS_DAEMON_PATH", title, 1);
-                }
-            }
-            execvp(mountprog, (char **) argv);
-            perror("fuse: failed to exec mount program");
-            exit(1);
         }
-
-        _exit(0);
+        execvp(mountprog, (char **) argv);
+        perror("fuse: failed to exec mount program");
+        exit(1);
     }
 
 out:
