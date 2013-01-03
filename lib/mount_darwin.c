9d17003efeb0d41d3a696ff2cb996fdb3af79f24
Benjamin Fleischer <fleiben@gmail.com>
2013-01-03 14:16:47 +0100

    Rename variable MOUNT_FUSEFS_DAEMON_PATH MOUNT_OSXFUSE_DAEMON_PATH

--- lib/mount_darwin.c
+++ lib/mount_darwin.c
@@ -690,7 +690,7 @@ mount:
             u_int32_t len = MAXPATHLEN;
             int ret = proc_pidpath(getpid(), title, len);
             if (ret) {
-                setenv("MOUNT_FUSEFS_DAEMON_PATH", title, 1);
+                setenv("MOUNT_OSXFUSE_DAEMON_PATH", title, 1);
             }
         }
         execvp(mountprog, (char **) argv);
