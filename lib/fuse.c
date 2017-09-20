osxfuse/fuse

commit ba5744cd6e2b801f0832d29ff6ee371443f5730c

Clean up support for very big file systems

lib/fuse.c
@@ -168,9 +168,6 @@ struct fuse {
 	struct list_head partial_slabs;
 	struct list_head full_slabs;
 	pthread_t prune_thread;
-#ifdef __APPLE__
-        int statfs_x_ok;
-#endif
 };
 
 struct lock {
@@ -2113,6 +2110,7 @@ int fuse_fs_statfs(struct fuse_fs *fs, const char *path, struct statvfs *buf)
 }
 
 #ifdef __APPLE__
+
 int fuse_fs_statfs_x(struct fuse_fs *fs, const char *path, struct statfs *buf)
 {
 	fuse_get_context()->private_data = fs->user_data;
@@ -2120,14 +2118,14 @@ int fuse_fs_statfs_x(struct fuse_fs *fs, const char *path, struct statfs *buf)
 		if (fs->debug)
 			fprintf(stderr, "statfs_x %s\n", path);
 
-               return fs->op.statfs_x(path, buf);
-               return 0;
+		return fs->op.statfs_x(path, buf);
 	} else {
 		buf->f_bsize = 512;
 		return 0;
 	}
 }
-#endif
+
+#endif /* __APPLE__ */
 
 int fuse_fs_releasedir(struct fuse_fs *fs, const char *path,
 		       struct fuse_file_info *fi)
@@ -4063,10 +4061,12 @@ static void fuse_lib_fsyncdir(fuse_req_t req, fuse_ino_t ino, int datasync,
 static void fuse_lib_statfs(fuse_req_t req, fuse_ino_t ino)
 {
 	struct fuse *f = req_fuse_prepare(req);
-	struct statvfs buf;
+	union {
+		struct statvfs statvfs;
 #ifdef __APPLE__
-        struct statfs buf_x;
+		struct statfs statfs;
 #endif
+	} buf;
 	char *path = NULL;
 	int err = 0;
 
@@ -4078,22 +4078,24 @@ static void fuse_lib_statfs(fuse_req_t req, fuse_ino_t ino)
 		struct fuse_intr_data d;
 		fuse_prepare_interrupt(f, req, &d);
 #ifdef __APPLE__
-                if(f->statfs_x_ok)
-			err = fuse_fs_statfs_x(f->fs, path ? path : "/", &buf_x);
+                if (f->fs->op.statfs_x)
+			err = fuse_fs_statfs_x(f->fs, path ? path : "/",
+					       &buf.statfs);
                 else
 #endif
-		err = fuse_fs_statfs(f->fs, path ? path : "/", &buf);
+			err = fuse_fs_statfs(f->fs, path ? path : "/",
+					     &buf.statvfs);
 		fuse_finish_interrupt(f, req, &d);
 		free_path(f, ino, path);
 	}
 
 	if (!err)
 #ifdef __APPLE__
-                if(f->statfs_x_ok)
-			fuse_reply_statfs_x(req, &buf_x);
+                if (f->fs->op.statfs_x)
+			fuse_reply_statfs_x(req, &buf.statfs);
                 else
 #endif
-		fuse_reply_statfs(req, &buf);
+			fuse_reply_statfs(req, &buf.statvfs);
 	else
 		reply_err(req, err);
 }
@@ -5183,10 +5185,6 @@ struct fuse *fuse_new_common(struct fuse_chan *ch, struct fuse_args *args,
 	f->nullpath_ok = fs->op.flag_nullpath_ok;
 	f->conf.nopath = fs->op.flag_nopath;
 	f->utime_omit_ok = fs->op.flag_utime_omit_ok;
-
-#ifdef __APPLE__
-        f->statfs_x_ok = fs->op.statfs_x ? 1 : 0;
-#endif
 
 	/* Oh f**k, this is ugly! */
 	if (!fs->op.lock) {

include/fuse.h
@@ -38,6 +38,10 @@
 #include <sys/statvfs.h>
 #include <sys/uio.h>
 
+#ifdef __APPLE__
+#  include <sys/mount.h>
+#endif
+
 #ifdef __cplusplus
 extern "C" {
 #endif
@@ -67,11 +71,6 @@ typedef int (*fuse_fill_dir_t) (void *buf, const char *name,
 typedef struct fuse_dirhandle *fuse_dirh_t;
 typedef int (*fuse_dirfil_t) (fuse_dirh_t h, const char *name, int type,
 			      ino_t ino);
-
-#ifdef __APPLE__
-/* Used by (*statfs_x) (const char *, struct statfs *) */
-struct statfs;
-#endif
 
 /**
  * The file system operations:
@@ -617,8 +616,8 @@ struct fuse_operations {
 			  void *, void *);
 	int (*reserved02)(void *, void *, void *, void *, void *, void *,
 			  void *, void *);
-	int (*reserved03)(void *, void *, void *, void *, void *, void *,
-			  void *, void *);
+
+	int (*statfs_x) (const char *, struct statfs *);
 
 	int (*setvolname) (const char *);
 
@@ -639,8 +638,6 @@ struct fuse_operations {
 
 	int (*fsetattr_x) (const char *, struct setattr_x *,
 			   struct fuse_file_info *);
-
-	int (*statfs_x) (const char *, struct statfs *);
 #endif /* __APPLE__ */
 };
 
@@ -907,7 +904,6 @@ int fuse_fs_flush(struct fuse_fs *fs, const char *path,
 int fuse_fs_statfs(struct fuse_fs *fs, const char *path, struct statvfs *buf);
 #ifdef __APPLE__
 int fuse_fs_statfs_x(struct fuse_fs *fs, const char *path, struct statfs *buf);
-
 #endif
 int fuse_fs_opendir(struct fuse_fs *fs, const char *path,
 		    struct fuse_file_info *fi);

include/fuse_common.h
@@ -8,7 +8,7 @@
 
 /*
  * Copyright (c) 2006-2008 Amit Singh/Google Inc.
- * Copyright (c) 2011-2012 Benjamin Fleischer
+ * Copyright (c) 2011-2017 Benjamin Fleischer
  */
 
 /** @file */
@@ -19,6 +19,20 @@
 
 #ifndef _FUSE_COMMON_H_
 #define _FUSE_COMMON_H_
+
+#ifdef __APPLE__
+
+/*
+ * The following integer types do not exist unless _DARWIN_C_SOURCE is
+ * defined. However, doing so would alter struct stat, therefore we need to
+ * define them ourselves.
+ */
+typedef unsigned char u_char;
+typedef unsigned short u_short;
+typedef unsigned int u_int;
+typedef unsigned long u_long;
+
+#endif /* __APPLE__ */
 
 #include "fuse_opt.h"
 #include <stdint.h>
@@ -159,11 +173,11 @@ struct fuse_file_info {
 #define FUSE_CAP_IOCTL_DIR	(1 << 11)
 #ifdef __APPLE__
 #  define FUSE_CAP_ALLOCATE		(1 << 27)
-#  define FUSE_CAP_EXCHANGE_DATA		(1 << 28)
+#  define FUSE_CAP_EXCHANGE_DATA	(1 << 28)
 #  define FUSE_CAP_CASE_INSENSITIVE	(1 << 29)
 #  define FUSE_CAP_VOL_RENAME		(1 << 30)
 #  define FUSE_CAP_XTIMES		(1 << 31)
-#endif
+#endif /* __APPLE__ */
 
 /**
  * Ioctl flags
@@ -260,9 +274,9 @@ struct fuse_conn_info {
    /*
     * Deprecated, use capability flags directly
     */
-#  define FUSE_ENABLE_SETVOLNAME(i)		(i)->want |= FUSE_CAP_VOL_RENAME
-#  define FUSE_ENABLE_XTIMES(i)			(i)->want |= FUSE_CAP_XTIMES
-#  define FUSE_ENABLE_CASE_INSENSITIVE(i)	(i)->want |= FUSE_CAP_CASE_INSENSITIVE
+#  define FUSE_ENABLE_SETVOLNAME(i)	  (i)->want |= FUSE_CAP_VOL_RENAME
+#  define FUSE_ENABLE_XTIMES(i)		  (i)->want |= FUSE_CAP_XTIMES
+#  define FUSE_ENABLE_CASE_INSENSITIVE(i) (i)->want |= FUSE_CAP_CASE_INSENSITIVE
 #endif
 
 struct fuse_session;

include/fuse_darwin.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2006-2008 Amit Singh/Google Inc.
- * Copyright (c) 2011-2012 Benjamin Fleischer
+ * Copyright (c) 2011-2017 Benjamin Fleischer
  */
 
 #ifdef __APPLE__
@@ -13,8 +13,6 @@ extern "C" {
 #endif
 
 #include <stdint.h>
-
-/* Versioning */
 
 const char *osxfuse_version(void);
 

include/fuse_lowlevel.h
@@ -38,18 +38,8 @@
 #include <sys/uio.h>
 
 #ifdef __APPLE__
-/*
- * The following integer types do not exist unless _DARWIN_C_SOURCE is
- * defined. However, doing so would alter struct stat, therefore we need
- * to define them ourselves.
- */
-typedef unsigned char u_char;
-typedef unsigned short u_short;
-typedef unsigned int u_int;
-typedef unsigned long u_long;
-
-#include <sys/mount.h>
-#endif /* __APPLE__ */
+#  include <sys/mount.h>
+#endif
 
 #ifdef __cplusplus
 extern "C" {
@@ -1080,8 +1070,6 @@ struct fuse_lowlevel_ops {
 	void (*setattr_x) (fuse_req_t req, fuse_ino_t ino,
 			   struct setattr_x *attr, int to_set,
 			   struct fuse_file_info *fi);
-
-	void (*statfs_x) (fuse_req_t req, fuse_ino_t ino);
 #endif /* __APPLE__ */
 };
 

lib/fuse_darwin_private.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2006-2008 Amit Singh/Google Inc.
- * Copyright (c) 2011-2012 Benjamin Fleischer
+ * Copyright (c) 2011-2017 Benjamin Fleischer
  */
 
 #ifdef __APPLE__
@@ -12,7 +12,6 @@
 extern "C" {
 #endif
 
-#include "fuse_lowlevel.h"
 #include "fuse_darwin.h"
 
 #include <fuse_param.h>

lib/fuse_lowlevel.c
@@ -1180,7 +1180,7 @@ static void do_setattr(fuse_req_t req, fuse_ino_t nodeid, const void *inarg)
 		stbuf.valid = arg->valid;
 		req->f->op.setattr_x(req, nodeid, &stbuf, arg->valid, fi);
 	} else
-#endif
+#endif /* __APPLE__ */
 	if (req->f->op.setattr) {
 		struct fuse_file_info *fi = NULL;
 		struct fuse_file_info fi_store;

lib/modules/iconv.c
@@ -564,6 +564,7 @@ static int iconv_statfs(const char *path, struct statvfs *stbuf)
 }
 
 #ifdef __APPLE__
+
 static int iconv_statfs_x(const char *path, struct statfs *stbuf)
 {
 	struct iconv *ic = iconv_get();
@@ -575,7 +576,8 @@ static int iconv_statfs_x(const char *path, struct statfs *stbuf)
 	}
 	return err;
 }
-#endif
+
+#endif /* __APPLE__ */
 
 static int iconv_flush(const char *path, struct fuse_file_info *fi)
 {
@@ -804,8 +806,9 @@ static const struct fuse_operations iconv_oper = {
 	.lock		= iconv_lock,
 	.flock		= iconv_flock,
 	.bmap		= iconv_bmap,
-	.fallocate   = iconv_fallocate,
+	.fallocate	= iconv_fallocate,
 #ifdef __APPLE__
+	.statfs_x	= iconv_statfs_x,
 	.setvolname	= iconv_setvolname,
 	.exchange	= iconv_exchange,
 	.getxtimes	= iconv_getxtimes,
@@ -815,8 +818,7 @@ static const struct fuse_operations iconv_oper = {
 	.chflags	= iconv_chflags,
 	.setattr_x	= iconv_setattr_x,
 	.fsetattr_x	= iconv_fsetattr_x,
-	.statfs_x	= iconv_statfs_x,
-#endif
+#endif /* __APPLE__ */
 
 	.flag_nullpath_ok = 1,
 	.flag_nopath = 1,

commit 559738e3535c1d999c4cf5c2426968cf3dfd4f8b

Fix statfs_x bug when using modules
This fixes a bug introduced in commit ba5744c. We need to track whether
the actual file system implements the high-level statfs_x callback or
not. Checking if f->op.statfs_x exists is not sufficient in case
modules are used.

lib/fuse.c
@@ -163,6 +163,9 @@ struct fuse {
 	struct fuse_fs *fs;
 	int nullpath_ok;
 	int utime_omit_ok;
+#ifdef __APPLE__
+	int statfs_x_ok;
+#endif
 	struct lock_queue_element *lockq;
 	int pagesize;
 	struct list_head partial_slabs;
@@ -4078,10 +4081,10 @@ static void fuse_lib_statfs(fuse_req_t req, fuse_ino_t ino)
 		struct fuse_intr_data d;
 		fuse_prepare_interrupt(f, req, &d);
 #ifdef __APPLE__
-                if (f->fs->op.statfs_x)
+		if (f->statfs_x_ok)
 			err = fuse_fs_statfs_x(f->fs, path ? path : "/",
 					       &buf.statfs);
-                else
+		else
 #endif
 			err = fuse_fs_statfs(f->fs, path ? path : "/",
 					     &buf.statvfs);
@@ -4091,7 +4094,7 @@ static void fuse_lib_statfs(fuse_req_t req, fuse_ino_t ino)
 
 	if (!err)
 #ifdef __APPLE__
-                if (f->fs->op.statfs_x)
+                if (f->statfs_x_ok)
 			fuse_reply_statfs_x(req, &buf.statfs);
                 else
 #endif
@@ -5086,6 +5089,9 @@ static int fuse_push_module(struct fuse *f, const char *module,
 	f->nullpath_ok = newfs->op.flag_nullpath_ok && f->nullpath_ok;
 	f->conf.nopath = newfs->op.flag_nopath && f->conf.nopath;
 	f->utime_omit_ok = newfs->op.flag_utime_omit_ok && f->utime_omit_ok;
+#ifdef __APPLE__
+	f->statfs_x_ok = newfs->op.statfs_x != NULL && f->statfs_x_ok;
+#endif
 	return 0;
 }
 
@@ -5185,6 +5191,9 @@ struct fuse *fuse_new_common(struct fuse_chan *ch, struct fuse_args *args,
 	f->nullpath_ok = fs->op.flag_nullpath_ok;
 	f->conf.nopath = fs->op.flag_nopath;
 	f->utime_omit_ok = fs->op.flag_utime_omit_ok;
+#ifdef __APPLE__
+	f->statfs_x_ok = fs->op.statfs_x != NULL;
+#endif
 
 	/* Oh f**k, this is ugly! */
 	if (!fs->op.lock) {