59629c7de83b42b58a20a60f13196e836ec01cb8
Charly Delay <delay@google.com>
2017-11-22 12:09:43 -0500

    Use kDADiskUnmountOptionForce instead of kDADiskUnmountOptionDefault
    Effectively apply the fix in 5473133ecef92879ee57b524e0a065215f819969.

--- lib/mount_darwin.c
+++ lib/mount_darwin.c
@@ -300,7 +300,7 @@ fuse_kern_unmount(DADiskRef disk, int fd)
 		return;
 	}
 
-	DADiskUnmount(disk, kDADiskUnmountOptionDefault, NULL, NULL);
+	DADiskUnmount(disk, kDADiskUnmountOptionForce, NULL, NULL);
 }
 
 void
