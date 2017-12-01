94deba2739c61e897d4df0e86d54ef4a3f824d06
Dave MacLachlan <dmaclach@gmail.com>
2017-12-01 13:05:36 -0800

    Move semaphore implementation on Darwin over to libdispatch.
    This cleans up fuse_darwin significantly, and moves the semaphore
    calls over on top of libdispatch. libdispatch has been around since
    10.6, so should be no compatibility problems.

--- lib/fuse_darwin.c
+++ lib/fuse_darwin.c
@@ -2,7 +2,8 @@
  * Copyright (c) 2006-2008 Amit Singh/Google Inc.
  * Copyright (c) 2012 Anatol Pomozov
  * Copyright (c) 2011-2017 Benjamin Fleischer
- */
+ * Copyright (c) 2017 Dave MacLachlan/Google Inc.
+*/
 
 #include "fuse_i.h"
 #include "fuse_darwin_private.h"
@@ -11,252 +12,55 @@
 #include <errno.h>
 #include <libgen.h>
 #include <mach-o/dyld.h>
-#include <pthread.h>
-#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/param.h>
-#include <sys/types.h>
 #include <unistd.h>
 
-#include <CoreFoundation/CoreFoundation.h>
-
-/*
- * Semaphore implementation based on:
- *
- * Copyright (C) 2000,02 Free Software Foundation, Inc.
- * This file is part of the GNU C Library.
- * Written by Ga<EB>l Le Mignot <address@hidden>
- *
- * The GNU C Library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
- *
- * The GNU C Library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Library General Public License for more details.
- *
- * You should have received a copy of the GNU Library General Public
- * License along with the GNU C Library; see the file COPYING.LIB.  If not,
- * write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
- */
-
-#include <assert.h>
-
-/* Semaphores */
-
-#define __SEM_ID_NONE  0x0
-#define __SEM_ID_LOCAL 0xcafef00d
 
 /* http://www.opengroup.org/onlinepubs/007908799/xsh/sem_init.html */
 int
-fuse_sem_init(fuse_sem_t *sem, int pshared, unsigned int value)
+fuse_sem_init(dispatch_semaphore_t *sem, int pshared, unsigned int value)
 {
 	if (pshared) {
 		errno = ENOSYS;
 		return -1;
 	}
 
-	sem->id = __SEM_ID_NONE;
-
-	if (pthread_cond_init(&sem->__data.local.count_cond, NULL)) {
-		goto cond_init_fail;
-	}
-
-	if (pthread_mutex_init(&sem->__data.local.count_lock, NULL)) {
-		goto mutex_init_fail;
-	}
-
-	sem->__data.local.count = value;
-	sem->id = __SEM_ID_LOCAL;
-
+  dispatch_semaphore_t local_sem = dispatch_semaphore_create(value);
+  if (local_sem == NULL) {
+    errno = ENOSPC;
+    return -1;
+  }
+  *sem = local_sem;
 	return 0;
-
-mutex_init_fail:
-
-	pthread_cond_destroy(&sem->__data.local.count_cond);
-
-cond_init_fail:
-
-	return -1;
 }
 
 /* http://www.opengroup.org/onlinepubs/007908799/xsh/sem_destroy.html */
 int
-fuse_sem_destroy(fuse_sem_t *sem)
+fuse_sem_destroy(dispatch_semaphore_t *sem)
 {
-	int res = 0;
-
-	pthread_mutex_lock(&sem->__data.local.count_lock);
-
-	sem->id = __SEM_ID_NONE;
-	pthread_cond_broadcast(&sem->__data.local.count_cond);
-
-	if (pthread_cond_destroy(&sem->__data.local.count_cond)) {
-		res = -1;
-	}
-
-	pthread_mutex_unlock(&sem->__data.local.count_lock);
-
-	if (pthread_mutex_destroy(&sem->__data.local.count_lock)) {
-		res = -1;
-	}
-
-	return res;
-}
-
-int
-fuse_sem_getvalue(fuse_sem_t *sem, unsigned int *sval)
-{
-	int res = 0;
-
-	pthread_mutex_lock(&sem->__data.local.count_lock);
-
-	if (sem->id != __SEM_ID_LOCAL) {
-		res = -1;
-		errno = EINVAL;
-	} else {
-		*sval = sem->__data.local.count;
-	}
-
-	pthread_mutex_unlock(&sem->__data.local.count_lock);
-
-	return res;
+  dispatch_release(*sem);
+  return 0;
 }
 
 /* http://www.opengroup.org/onlinepubs/007908799/xsh/sem_post.html */
 int
-fuse_sem_post(fuse_sem_t *sem)
+fuse_sem_post(dispatch_semaphore_t *sem)
 {
-	int res = 0;
-
-	pthread_mutex_lock(&sem->__data.local.count_lock);
-
-	if (sem->id != __SEM_ID_LOCAL) {
-		res = -1;
-		errno = EINVAL;
-	} else if (sem->__data.local.count < FUSE_SEM_VALUE_MAX) {
-		sem->__data.local.count++;
-		if (sem->__data.local.count == 1) {
-			pthread_cond_signal(&sem->__data.local.count_cond);
-		}
-	} else {
-		errno = ERANGE;
-		res = -1;
-	}
-
-	pthread_mutex_unlock(&sem->__data.local.count_lock);
-
-	return res;
-}
-
-/* http://www.opengroup.org/onlinepubs/009695399/functions/sem_timedwait.html */
-int
-fuse_sem_timedwait(fuse_sem_t *sem, const struct timespec *abs_timeout)
-{
-	int res = 0;
-
-	if (abs_timeout &&
-	    (abs_timeout->tv_nsec < 0 || abs_timeout->tv_nsec >= 1000000000)) {
-		errno = EINVAL;
-		return -1;
-	}
-
-	pthread_cleanup_push((void(*)(void*))&pthread_mutex_unlock,
-			     &sem->__data.local.count_lock);
-
-	pthread_mutex_lock(&sem->__data.local.count_lock);
-
-	if (sem->id != __SEM_ID_LOCAL) {
-		errno = EINVAL;
-		res = -1;
-	} else {
-		if (!sem->__data.local.count) {
-			res = pthread_cond_timedwait(&sem->__data.local.count_cond,
-						     &sem->__data.local.count_lock,
-						     abs_timeout);
-		}
-		if (res) {
-			assert(res == ETIMEDOUT);
-			res = -1;
-			errno = ETIMEDOUT;
-		} else if (sem->id != __SEM_ID_LOCAL) {
-			res = -1;
-			errno = EINVAL;
-		} else {
-			sem->__data.local.count--;
-		}
-	}
-
-	pthread_cleanup_pop(1);
-
-	return res;
-}
-
-/* http://www.opengroup.org/onlinepubs/007908799/xsh/sem_trywait.html */
-int
-fuse_sem_trywait(fuse_sem_t *sem)
-{
-	int res = 0;
-
-	pthread_mutex_lock(&sem->__data.local.count_lock);
-
-	if (sem->id != __SEM_ID_LOCAL) {
-		res = -1;
-		errno = EINVAL;
-	} else if (sem->__data.local.count) {
-		sem->__data.local.count--;
-	} else {
-		res = -1;
-		errno = EAGAIN;
-	}
-
-	pthread_mutex_unlock (&sem->__data.local.count_lock);
-
-	return res;
+  dispatch_semaphore_signal(*sem);
+	return 0;
 }
 
 /* http://www.opengroup.org/onlinepubs/007908799/xsh/sem_wait.html */
 int
-fuse_sem_wait(fuse_sem_t *sem)
+fuse_sem_wait(dispatch_semaphore_t *sem)
 {
-	int res = 0;
-
-	pthread_cleanup_push((void(*)(void*))&pthread_mutex_unlock,
-			     &sem->__data.local.count_lock);
-
-	pthread_mutex_lock(&sem->__data.local.count_lock);
-
-	if (sem->id != __SEM_ID_LOCAL) {
-		errno = EINVAL;
-		res = -1;
-	} else {
-		if (!sem->__data.local.count) {
-			pthread_cond_wait(&sem->__data.local.count_cond,
-					  &sem->__data.local.count_lock);
-			if (!sem->__data.local.count) {
-				/* spurious wakeup, assume it is an interruption */
-				res = -1;
-				errno = EINTR;
-				goto out;
-			}
-		}
-		if (sem->id != __SEM_ID_LOCAL) {
-			res = -1;
-			errno = EINVAL;
-		} else {
-			sem->__data.local.count--;
-		}
-	}
-
-out:
-	pthread_cleanup_pop(1);
-
-	return res;
+  if (dispatch_semaphore_wait(*sem, DISPATCH_TIME_FOREVER) != 0) {
+    errno = EINTR;
+    return -1;
+  }
+	return 0;
 }
 
 /********************/
